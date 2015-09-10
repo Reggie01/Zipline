efcd67b510e1e3a69a873c4a0aa168917913c420
Reggie S <reggie@bgcreatives.com>
2015-09-10 17:31:25 -0400

    added timer functionality, sound effects, and css to improve look

--- Zipline/PomodoroClock/index.js
+++ Zipline/PomodoroClock/index.js
@@ -1,13 +1,12 @@
-﻿$(document).ready(function() {
+﻿/* Sound effects from http://www.freesfx.co.uk */
+$(document).ready(function() {
 
-    var start = null;
-    var element = document.getElementById("SomeElementYouWantToAnimate");
     var elemMinutes = document.getElementsByClassName("pomodoro-time-minute")[0];
     var elemSeconds = document.getElementsByClassName("pomodoro-time-seconds")[0];
-    var seconds = 0;
     var totalSeconds = +elemSeconds.textContent;
     var totalMinutes = +elemMinutes.textContent;
-   
+    var v = document.getElementsByTagName("audio")[0];
+    
     function addZeros(match) {
         if(match.length < 2){
           return "0" + match;
@@ -15,52 +14,83 @@
         return match;
     };
     
-    function calculateSeconds(timePassed) {
+    function calculateSeconds() {
           
-       seconds = Math.floor(timePassed/1000) % 60;
-       console.log(seconds);
-       if(totalMinutes > 0 && seconds === 0){ 
-          console.log("hello");
-           --totalMinutes;
+       if(totalMinutes > 0 && +elemSeconds.textContent === 0){ 
            totalSeconds = 59;
-           console.log(totalMinutes);
+           elemSeconds.textContent = (totalSeconds).toString(); 
+           --totalMinutes;
            elemMinutes.textContent = (totalMinutes).toString();
-           elemSeconds.textContent = addZeros((totalSeconds - seconds).toString());    
-                                     
+                                                  
        } else {
-           if(totalSeconds === 0 && totalMinutes <= 0){
+           if(+elemSeconds.textContent === 0 && totalMinutes <= 0){
                console.log("The end");
-           } else {
-                
-                elemSeconds.textContent = addZeros((totalSeconds - seconds).toString());
                
+               if($(".pomodoro-text").text() === "Session") {
+                    $(".pomodoro-text").text("");
+                    $(".pomodoro-text").text("Break!");
+                    $(".pomodoro-time-minute").text($(".time-break-numeral").text());
+                    totalMinutes = parseInt(+$(".pomodoro-time-minute").text());
+                    console.log(document.getElementsByTagName("audio"));                   
+                    v.play();
+               }
+               else if($(".pomodoro-text").text() === "Break!") {
+                    $(".pomodoro-text").text("Session");
+                    $(".pomodoro-time-minute").text($(".time-break-numeral").text());
+                    totalMinutes = parseInt(+$(".pomodoro-time-minute").text());
+                    v.play();
+               }
+               
+           } else {                
+                elemSeconds.textContent = addZeros((--totalSeconds).toString());               
            }                                  
        }       
     }
     
-    function step(timestamp) {
-
-        if (!start) start = timestamp;
-        var progress = timestamp - start;
-        element.style.left = Math.min(progress/10, 200) + "px";
-        
-       calculateSeconds(progress);
-       
-        if (progress < 120000) {
-            id = window.requestAnimationFrame(step);
-        }
+    function step(timestamp) {        
+       calculateSeconds();           
     }
-
-
+   
    var id = 0;
-   $(".pomodoro").click(function() {      
+  
+   $(".pomodoro").click(function() {    
       if(id) {
-         window.cancelAnimationFrame(id);
-         id = 0;
-      } else { 
-         id = window.requestAnimationFrame(step);
+           clearInterval(id);
+           id = 0;
+      } else {
+           id = setInterval(function() { 
+   
+           step(); }, 1000);
       }
       
-   })
+   });
    
+   $(".time-session-minus").click(function() {
+        var currentTime = parseInt($(".time-session-numeral").text());
+        if(currentTime - 1 > 0){
+         $(".time-session-numeral").text(currentTime - 1);
+         $(".pomodoro-time-minute").text($(".time-session-numeral").text());
+         totalMinutes =  $(".pomodoro-time-minute").text();
+        }        
+   });
+   
+   $(".time-session-plus").click(function() {
+        var currentTime = parseInt($(".time-session-numeral").text());
+        $(".time-session-numeral").text(currentTime + 1);
+        $(".pomodoro-time-minute").text($(".time-session-numeral").text());
+        totalMinutes =  $(".pomodoro-time-minute").text(); 
+   });
+   
+   
+   $(".time-break-minus").click(function() {
+        var currentTime = parseInt($(".time-break-numeral").text());
+        if(currentTime - 1 >= 0){
+         $(".time-break-numeral").text(currentTime - 1);
+        }        
+   });
+   
+   $(".time-break-plus").click(function() {
+        var currentTime = parseInt($(".time-break-numeral").text());
+        $(".time-break-numeral").text(currentTime + 1);
+   });
 });
