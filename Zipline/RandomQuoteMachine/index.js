7cbce9ffecbd72a6d82dfe5cea070a1d6b982b1f
Reggie S <reggie@bgcreatives.com>
2015-09-30 18:46:21 -0400

    Create modular js.
    Remove globals from javascript code by using modules.

--- Zipline/RandomQuoteMachine/index.js
+++ Zipline/RandomQuoteMachine/index.js
@@ -1,9 +1,24 @@
 ﻿$(document).ready(function() {
-
-    var tweetButton = document.getElementById('tweet-button');
-
-    function postQuote() {
-        $.ajax({
+    "use strict";
+    
+    var AjaxErrorHandler = (function() {
+         return{
+             errorHandler : function(xhr, status, errorThrown) {
+                 console.log("Sorry, there was a problem!");
+                 console.log("Error: " + errorThrown);
+                 console.log("Status: " + status);
+                 console.dir(xhr);
+             } ,
+             alwaysHandler : function(xhr, status) {
+                 //console.log(xhr);
+             }
+         };
+    })();
+    
+    var TweetModule = (function(ajaxHandler) {
+       var tweetButton = document.getElementById('tweet-button');
+       var ajaxCall = function() {
+            return $.ajax({
                 url: "http://api.forismatic.com/api/1.0/",
 
                 dataType: "jsonp",
@@ -15,32 +30,31 @@
                     lang: "en",
                     jsonp: "callback"
                 }
-            }).
-            // Work with the response
-        done(function(response) {
-            $("#new-quote").html('"' + response.quoteText + '"');
-            $("#author-quote").html(response.quoteAuthor);
-            tweetButton.src = tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(response.quoteText + " - " + response.quoteAuthor));
-        }).
-        fail(function(xhr, status, errorThrown) {
-                console.log("Sorry, there was a problem!");
-                console.log("Error: " + errorThrown);
-                console.log("Status: " + status);
-                console.dir(xhr);
-            }).
-            // Code to run regardless of success or failure
-        always(function(xhr, status) {
-            
-            //console.log(xhr);
-        });
-    }
+            });
+       }; 
+       return {
+            updateDom : function(response) {
+                $("#new-quote").html('"' + response.quoteText + '"');
+                $("#author-quote").html(response.quoteAuthor);
+                tweetButton.src = tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(response.quoteText + " - " + response.quoteAuthor));
+            },
+            getAjaxCall : function() {
+                 return ajaxCall();
+            },
+           getQuote: function postQuote() {
+               TweetModule.getAjaxCall().
+               done(TweetModule.updateDom).
+               fail(ajaxHandler.errorHandler).
+               always(ajaxHandler.alwaysHandler);
+           }  
+       };
+    })(AjaxErrorHandler);
 
-    postQuote();
+    TweetModule.getQuote();
 
     $("#quote").click(function(e) {
         e.preventDefault();
-        postQuote();
-
+        TweetModule.getQuote();
     });
 
 });
