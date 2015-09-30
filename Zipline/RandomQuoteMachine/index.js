3766d9f7ba81c56680ea2d7e7685f4e770e909d4
Reggie S <reggie@bgcreatives.com>
2015-09-30 18:56:16 -0400

    Create app with dom and ajax methods private

--- Zipline/RandomQuoteMachine/index.js
+++ Zipline/RandomQuoteMachine/index.js
@@ -1,23 +1,23 @@
 ﻿$(document).ready(function() {
     "use strict";
-    
+
     var AjaxErrorHandler = (function() {
-         return{
-             errorHandler : function(xhr, status, errorThrown) {
-                 console.log("Sorry, there was a problem!");
-                 console.log("Error: " + errorThrown);
-                 console.log("Status: " + status);
-                 console.dir(xhr);
-             } ,
-             alwaysHandler : function(xhr, status) {
-                 //console.log(xhr);
-             }
-         };
+        return {
+            errorHandler: function(xhr, status, errorThrown) {
+                console.log("Sorry, there was a problem!");
+                console.log("Error: " + errorThrown);
+                console.log("Status: " + status);
+                console.dir(xhr);
+            },
+            alwaysHandler: function(xhr, status) {
+                //console.log(xhr);
+            }
+        };
     })();
-    
+
     var TweetModule = (function(ajaxHandler) {
-       var tweetButton = document.getElementById('tweet-button');
-       var ajaxCall = function() {
+        var _tweetButton = document.getElementById('tweet-button');
+        var _ajaxCall = function() {
             return $.ajax({
                 url: "http://api.forismatic.com/api/1.0/",
 
@@ -31,23 +31,21 @@
                     jsonp: "callback"
                 }
             });
-       }; 
-       return {
-            updateDom : function(response) {
-                $("#new-quote").html('"' + response.quoteText + '"');
-                $("#author-quote").html(response.quoteAuthor);
-                tweetButton.src = tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(response.quoteText + " - " + response.quoteAuthor));
-            },
-            getAjaxCall : function() {
-                 return ajaxCall();
-            },
-           getQuote: function postQuote() {
-               TweetModule.getAjaxCall().
-               done(TweetModule.updateDom).
-               fail(ajaxHandler.errorHandler).
-               always(ajaxHandler.alwaysHandler);
-           }  
-       };
+        };
+        var _updateDom = function(response) {
+            $("#new-quote").html('"' + response.quoteText + '"');
+            $("#author-quote").html(response.quoteAuthor);
+            _tweetButton.src = _tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(response.quoteText + " - " + response.quoteAuthor));
+        };
+
+        return {
+            getQuote: function postQuote() {
+                _ajaxCall().
+                done(_updateDom).
+                fail(ajaxHandler.errorHandler).
+                always(ajaxHandler.alwaysHandler);
+            }
+        };
     })(AjaxErrorHandler);
 
     TweetModule.getQuote();
