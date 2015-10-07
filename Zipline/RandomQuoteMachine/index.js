377ce517716fc8c0ca1caec538a7bb382a621430
Reggie S <reggie@bgcreatives.com>
2015-10-07 13:46:19 -0400

    Add whitespace to app to increase readability

--- Zipline/RandomQuoteMachine/index.js
+++ Zipline/RandomQuoteMachine/index.js
@@ -1,22 +1,22 @@
-﻿$(document).ready(function() {
+﻿$( document ).ready( function() {
     "use strict";
 
-    var AjaxErrorHandler = (function() {
+    var AjaxErrorHandler = ( function() {
         return {
-            errorHandler: function(xhr, status, errorThrown) {
-                console.log("Sorry, there was a problem!");
-                console.log("Error: " + errorThrown);
-                console.log("Status: " + status);
-                console.dir(xhr);
+            errorHandler: function( xhr, status, errorThrown ) {
+                console.log( "Sorry, there was a problem!" );
+                console.log( "Error: " + errorThrown );
+                console.log( "Status: " + status );
+                console.dir( xhr );
             },
-            alwaysHandler: function(xhr, status) {
-                //console.log(xhr);
+            alwaysHandler: function( xhr, status ) {
+                //console.log( xhr );
             }
         };
     })();
 
-    var TweetModule = (function(ajaxHandler) {
-        var _tweetButton = document.getElementById('tweet-button');
+    var TweetModule = ( function( ajaxHandler ) {
+        var _tweetButton = document.getElementById( 'tweet-button' );
         var _ajaxCall = function() {
             return $.ajax({
                 url: "http://api.forismatic.com/api/1.0/",
@@ -32,25 +32,25 @@
                 }
             });
         };
-        var _updateDom = function(response) {
-            $("#new-quote").html('"' + response.quoteText + '"');
-            $("#author-quote").html(response.quoteAuthor);
-            _tweetButton.src = _tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(response.quoteText + " - " + response.quoteAuthor));
+        var _updateDom = function( response ) {
+            $("#new-quote").html( '"' + response.quoteText + '"' );
+            $("#author-quote").html( response.quoteAuthor );
+            _tweetButton.src = _tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent( response.quoteText + " - " + response.quoteAuthor ));
         };
 
         return {
             getQuote: function postQuote() {
                 _ajaxCall().
-                done(_updateDom).
-                fail(ajaxHandler.errorHandler).
-                always(ajaxHandler.alwaysHandler);
+                done( _updateDom ).
+                fail( ajaxHandler.errorHandler ).
+                always( ajaxHandler.alwaysHandler );
             }
         };
-    })(AjaxErrorHandler);
+    })( AjaxErrorHandler );
 
     TweetModule.getQuote();
 
-    $("#quote").click(function(e) {
+    $( "#quote" ).click( function(e) {
         e.preventDefault();
         TweetModule.getQuote();
     });
