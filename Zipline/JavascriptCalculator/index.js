7a8db4e3069f31df905e160d39bbc15043667628
Reggie S <reggie@bgcreatives.com>
2015-10-09 08:32:05 -0400

    Initial set up of calculator app

---
+++ Zipline/JavascriptCalculator/index.js
@@ -0,0 +1,37 @@
+﻿$( document ).ready( function() {
+    
+    var str = "";
+    
+    var calcButtons = {
+         1: 1,
+         2: 2,
+         3: 3,
+         4: 4,
+         5: 5,
+         6: 6, 
+         7: 7,
+         8: 8,
+         9: 9,
+      "+": function(arr) ( return arr.reduce(function(elem) { return a + b; } )),
+      "-": function() { },
+      "\\": function() {},
+      "x": function() {},
+      ".": function() {},
+     "=": function() {},
+     "%": function() {},
+    "CE": function() {},
+    "CA": function(elem) { elem.val(); }
+    }
+    
+    $( ".calc-wrapper" ).on("click", "button", function(e) {
+        console.log(e);
+        
+        $( "input" ).val(e.target.innerText);
+        
+                
+        str += e.target.innerText;
+        
+        
+        console.log(str);
+    })
+} );
