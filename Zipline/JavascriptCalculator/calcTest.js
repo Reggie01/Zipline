1437abb5a8c8b7541af849c852a2adb3c074584f
Reggie S <reggie@bgcreatives.com>
2015-10-10 23:58:03 -0400

    Create calc app w\ tests for CE,C,.,numbers
    Added test for functions. Buttons working properly for CE, C, ., numbers.

---
+++ Zipline/JavascriptCalculator/calcTest.js
@@ -0,0 +1,35 @@
+﻿var results = {
+    total: 0,
+    bad: 0
+  };
+
+  console.log(calcModule);
+  
+function test(nameOfTest, then, expected) {
+    results.total++;
+    var result = calcModule.handleInput(then);
+    if (result !== expected) {
+      results.bad++;
+      console.log(nameOfTest + "Expected " + expected +
+        ", but was " + result);
+    }
+  }
+  
+  // testing for numbers
+  test( "Test input 0: ", 0, "0");
+  test("Test input 100: ",100, "0100");
+  test("Test clear input: ","C", "0");
+  test("Test input 100: ",100, "100");
+  test("Test input 1: ", 1, "1001");
+  test("Test input CE: ", "CE", "0");
+  test("Test input period  : ", ".", "0.");
+  test("Test repetitive period input : ",".", "0.");
+  test("Test input 1: ", "1", "0.1");
+  test("Test input 0: ", "0", "0.10");
+  test("Test input . : ", ".", "0.10");
+  test("Test input 2: ", "2", "0.102");
+  test("Test input CE: ", "CE", "0");  
+  
+  console.log("Of " + results.total + " tests, " +
+    results.bad + " failed, " +
+    (results.total - results.bad) + " passed.");
--- Zipline/JavascriptCalculator/index.js
+++ Zipline/JavascriptCalculator/index.js
@@ -1,37 +1,83 @@
-﻿$( document ).ready( function() {
-    
-    var str = "";
-    
-    var calcButtons = {
-         1: 1,
-         2: 2,
-         3: 3,
-         4: 4,
-         5: 5,
-         6: 6, 
-         7: 7,
-         8: 8,
-         9: 9,
-      "+": function(arr) ( return arr.reduce(function(elem) { return a + b; } )),
-      "-": function() { },
-      "\\": function() {},
-      "x": function() {},
-      ".": function() {},
-     "=": function() {},
-     "%": function() {},
-    "CE": function() {},
-    "CA": function(elem) { elem.val(); }
-    }
-    
-    $( ".calc-wrapper" ).on("click", "button", function(e) {
-        console.log(e);
-        
-        $( "input" ).val(e.target.innerText);
-        
+﻿var calcModule = (function () {
+
+    var calcVariables = [];
+
+    function handleInput(str) {
+        var updateStr = '';
+        var isStrNum = !isNaN(str);
+        if (isStrNum) {
+            calcVariables.push(+str);
+            return updateStr = calcVariables.join("");
+        }
+
+        if (str === "C") {
+            calcVariables = [];
+            return "0";
+        } else if (str === "CE") {
+            if (calcVariables.length > 0) {
+                if(!isNaN(+calcVariables.join(""))){
+                     calcVariables = [];
+                     return "0";
+                }
+                var re = /\+\-|\/|\*/g;
+                if( re.test( calcVariables.join("") ) ) {
+                     updateStr = calcVariables.join("");
+                }
                 
-        str += e.target.innerText;
-        
-        
+            } else {
+                return "0";
+            }
+        } else if (str === ".") {
+            console.log(calcVariables.length);
+            if (calcVariables.length > 0) {
+                if (calcVariables.join("").indexOf(".") !== -1) {
+                    console.log(calcVariables);
+                    return calcVariables.join("");
+                }
+                var calcVariable = calcVariables[calcVariables.length - 1];
+                var isNum = isNaN(+calcVariable);
+                if (!isNum) {
+                    calcVariables.push(str);
+                    return calcVariable + str;
+                } else {
+                    calcVariables.push("0" + str);
+                    return "0" + str;
+                }
+            } else {
+                calcVariables.push("0.");
+                return "0" + str;
+            }
+
+        }
+
+        return updateStr;
+    }
+
+    function getCalcVariables() {
+        return calcVariables;
+    }
+
+    return {
+        handleInput : handleInput,
+        getCalcVariables : getCalcVariables
+    }
+}
+    ());
+
+$(document).ready(function () {
+
+    var input$ = $("input");
+    input$.val("0");
+
+    $(".calc-wrapper").on("click", "button", function (e) {
+
+        str = e.target.innerText;
         console.log(str);
+        console.log(calcModule.getCalcVariables());
+
+        var newStr = calcModule.handleInput(str);
+
+        console.log(calcModule.getCalcVariables());
+        input$.val(newStr);
     })
-} );
+});
